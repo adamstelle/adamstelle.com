bb4e21a0555b1d33c11aaed5eb4f759430f71476
Adam Stelle <adamstelle@gmail.com>
2012-07-28 13:54:56 -0700

    restructured to sinatra app

---
+++ app.rb
@@ -0,0 +1,33 @@
+#Rubygems
+require 'rubygems'
+
+#Sinatra
+require 'sinatra'
+require 'sinatra/content_for'
+require 'sinatra/reloader'
+require 'sinatra/flash'
+
+
+# Serve all public folder files
+# set :public_folder, 'public'
+
+# Routing
+
+get '/' do
+  haml :index
+end
+
+get '/:route' do
+  valid_routes = [  'about' ] 
+  
+  valid_routes.each do | r |
+    @route = r if params[:route] == r
+  end
+  halt 404 unless @route
+  haml @route.to_sym
+end
+  
+get '/stylesheets/*' do
+  content_type 'text/css'
+  sass '../styles/'.concat(params[:splat].join.chomp('.css')).to_sym
+end
