51a5ce102d5acbc27bb043025c98e9da535bbeed
Adam Stelle <adamstelle@gmail.com>
2012-07-28 12:35:32 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AdamstelleCom::Application.config.secret_token = '558180e3248bf278b576b6a60ee56d835c6e53eeba97a4edd4bb0ee1afbc20ba7104d4cc9c80c2c9af015f80cba81b04e1dbeb7040587c3a17a2135aa51a2fec'
